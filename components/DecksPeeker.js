cecf892ea81ad95745804dc759ef3072ae78dcd1
Mark Anderson <udacity.com@ma7.org>
2017-09-24 13:29:03 -0500

    feat: Styled Deck List

--- components/DecksPeeker.js
+++ components/DecksPeeker.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import {
+  FlatList,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { connect } from 'react-redux';
 import { receiveDecks, addDeck } from '../actions';
 import { fetchDecks } from '../utils/api';
+import DecksListItem from './DecksListItem';
+import { white, blue } from '../utils/colors';
 
 class DecksPeeker extends Component {
   componentDidMount() {
@@ -15,13 +24,24 @@ class DecksPeeker extends Component {
       })
       .then(() => this.setState(() => ({ ready: true })));
   }
+  renderItem({ item }) {
+    const { title, questions } = item;
+    console.log('renderItem title', title);
+    return (
+      <View style={styles.item}>
+        <TouchableOpacity onPress={() => console.log('Pressed!')}>
+          <DecksListItem title={title} questions={questions} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
   render() {
     const { decks } = this.props;
     return (
       <View>
         <FlatList
-          data={Object.keys(decks)}
-          renderItem={({ item }) => <Text>{item}</Text>}
+          data={Object.values(decks)}
+          renderItem={this.renderItem}
           keyExtractor={(item, index) => index}
         />
       </View>
@@ -34,5 +54,24 @@ function mapStateToProps(state) {
     decks: state,
   };
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
+});
 
 export default connect(mapStateToProps)(DecksPeeker);
